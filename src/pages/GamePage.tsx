18338edfc2ac25b283ad806859135c8968cac934
L <lmwoo0327@naver.com>
2025-07-18 17:27:58 +0900

    chore: tailwind 추가

---
+++ src/pages/GamePage.tsx
@@ -0,0 +1,16 @@
+import { useParams } from "react-router-dom";
+import Tetris from "../games/tetris/Tetris";
+import Snake from "../games/snake/Snake";
+
+export default function GamePage() {
+  const { name } = useParams();
+
+  switch (name) {
+    case "tetris":
+      return <Tetris />;
+    case "snake":
+      return <Snake />;
+    default:
+      return <div>게임을 찾을 수 없습니다.</div>;
+  }
+}
